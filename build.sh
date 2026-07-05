a03777f5266520635c79db28e17d921964d5bab5
VARUN K A <varun.k.a.1005@gmail.com>
2026-07-05 16:36:50 +0530

    Fix build script

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ User = get_user_model()
 if not User.objects.filter(username="varun").exists():
     User.objects.create_superuser(
         "varun",
-        "varun.k.a.1005@gmal.com",
+        "varun.k.a.1005@gmail.com",
         "Varun51130"
     )
 END
